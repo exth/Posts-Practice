cfe94afd6083ccde2583b455da83aa61c39fd550
Вадим <exth331@gmail.com>
2026-06-23 06:07:37 +0300

    Add Xcode file

---
+++ postsPractice/postsPractice/postsPracticeApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+
+@main
+struct postsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ postsPractice/postsPracticeTests/postsPracticeTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+@testable import postsPractice
+
+
+final class postsPracticeTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown()  {
+        
+    }
+
+    func testExample() throws {
+
+    }
+}
